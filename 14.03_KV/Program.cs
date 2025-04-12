223eca25fc44e949aea355a67b768d30455efad4
kutail <kutep0v.vlad.m@gmail.com>
2025-04-13 00:25:14 +0500

    Добавьте файлы проекта.

---
+++ 14.03_KV/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _14._03_KV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+        {
+            new Student { Name = "Юлька", Faculty = "Математика", Grades = new List<int> { 2, 4, 4, 5 } },
+            new Student { Name = "Владик", Faculty = "Математика", Grades = new List<int> { 5, 4, 5, 5 } },
+            new Student { Name = "Юлька", Faculty = "Русский", Grades = new List<int> { 5, 3, 5, 5 } },
+            new Student { Name = "Владик", Faculty = "Русский", Grades = new List<int> { 5, 3, 4, 3 } },
+        };
+
+            var facultyGroups = students.GroupBy(s => s.Faculty);
+
+            foreach (var facultyGroup in facultyGroups)
+            {
+                double facultyAverage = facultyGroup.Average(s => s.AverageGrade);
+                var bestStudent = facultyGroup.OrderByDescending(s => s.AverageGrade).First();
+                Console.WriteLine($"Факультет: {facultyGroup.Key}");
+                Console.WriteLine($"Средний балл: {facultyAverage}");
+                Console.WriteLine($"Лучший студент: {bestStudent.Name} (Средний балл: {bestStudent.AverageGrade})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 14.03_KV/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._03_KV
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public List<int> Grades { get; set; }
+        public double AverageGrade
+        {
+            get { return Grades.Average(); } // не понял почему не работает в стандартной форме public double AverageGrade(){...};
+        }
+    }
+}
